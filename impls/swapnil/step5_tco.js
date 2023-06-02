82bdb1fedbddb1cfefc587ed4e1ea67c4e1a6fca
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2023-06-02 15:25:30 +0530

    [WIP] Implement tail call optimization

---
+++ impls/swapnil/step5_tco.js
@@ -0,0 +1,140 @@
+const readline = require("readline");
+const { readString } = require("./reader");
+const { printString } = require("./printer");
+const { MalSymbol, MalList, MalVector, MalNil } = require("./types");
+const { Env } = require("./env");
+const { ns } = require("./core");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const READ = (input) => readString(input);
+
+const createClosureFunction = (env, ast) => {
+  return (...args) => {
+    const fnEnv = new Env(env);
+    const bindings = ast.value[1].value;
+
+    bindings.forEach((binding, index) => fnEnv.set(binding, args[index]));
+    return EVAL(ast.value[2], fnEnv);
+  };
+};
+
+const evalDo = (ast, env) => {
+  const doLists = ast.value.slice(1);
+
+  doLists.slice(0, -1).forEach((list) => EVAL(list, env));
+
+  return doLists[doLists.length - 1];
+};
+
+const evalIf = (ast, env) => {
+  const predicateResult = EVAL(ast.value[1], env);
+
+  const listToExecute =
+    !(predicateResult instanceof MalNil) && predicateResult !== false
+      ? ast.value[2]
+      : ast.value[3];
+
+  return listToExecute !== undefined ? listToExecute : new MalNil();
+};
+
+const evalLetStar = (env, ast) => {
+  const letEnv = new Env(env);
+  const bindingList = ast.value[1];
+
+  for (let index = 0; index < bindingList.value.length; index += 2) {
+    const binding = bindingList.value[index];
+    const value = bindingList.value[index + 1];
+    letEnv.set(binding, EVAL(value, letEnv));
+  }
+
+  const form = ast.value[2] ? ast.value[2] : new MalNil();
+  return [form, letEnv];
+};
+
+const evalAst = (ast, env) => {
+  if (ast instanceof MalSymbol) {
+    return env.get(ast);
+  }
+
+  if (ast instanceof MalList) {
+    const newAst = ast.value.map((x) => EVAL(x, env));
+    return new MalList(newAst);
+  }
+
+  if (ast instanceof MalVector) {
+    const newAst = ast.value.map((x) => EVAL(x, env));
+    return new MalVector(newAst);
+  }
+
+  return ast;
+};
+
+const EVAL = (ast, env) => {
+  while (true) {
+    if (!(ast instanceof MalList)) {
+      return evalAst(ast, env);
+    }
+
+    if (ast.isEmpty()) {
+      return ast;
+    }
+
+    switch (ast.value[0].value) {
+      case "def!":
+        env.set(ast.value[1], EVAL(ast.value[2], env));
+        return env.get(ast.value[1]);
+
+      case "let*":
+        [ast, env] = evalLetStar(env, ast);
+        break;
+
+      case "do":
+        ast = evalDo(ast, env);
+        break;
+
+      case "if":
+        ast = evalIf(ast, env);
+        break;
+
+      case "fn*":
+        const closureFn = createClosureFunction(env, ast);
+        closureFn.toString = () => "#function";
+        return closureFn;
+      default:
+        const [fn, ...args] = evalAst(ast, env).value;
+        return fn.apply(null, args);
+    }
+  }
+};
+
+const PRINT = (malValue) => printString(malValue);
+
+const initEnv = () => {
+  const env = new Env();
+
+  for (const symbol in ns) {
+    env.set(new MalSymbol(symbol), ns[symbol]);
+  }
+
+  return env;
+};
+
+const env = initEnv();
+
+const rep = (input) => PRINT(EVAL(READ(input), env));
+
+const repl = () =>
+  rl.question("user> ", (line) => {
+    try {
+      console.log(rep(line));
+    } catch (e) {
+      console.log(e);
+    }
+    repl();
+  });
+
+repl();
